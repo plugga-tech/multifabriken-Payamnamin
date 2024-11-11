05a583be7c59b61801dddbdf6aa1b83bacb7c2bd
Payam <payamnamin@programmer.net>
2024-11-11 15:42:06 +0100

    Create Product.java
    Create Product class, methods and  constructor

---
+++ Product.java
@@ -0,0 +1,12 @@
+public class Product {
+    private String type;
+
+
+    public Product(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
