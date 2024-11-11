3717822025b321a12c4760582f0b41a1f47ac519
Payam <payamnamin@programmer.net>
2024-11-11 18:24:03 +0100

    Create App.java
    add variables

---
+++ App.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+    
+    private static List<Car> cars = new ArrayList<>(null);
+    private static List<Candy> candy = new ArrayList<>(null);
+    private static List<Pipe> pipe = new ArrayList<>(null);
+    private static List<Oatmilk> oatmilks = new ArrayList<>(null);
+
+}
